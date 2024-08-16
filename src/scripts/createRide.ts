bd490a1b7b27f84d50d1b9b88663130b888e8ddd
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-08-16 16:25:12 -0700

    created first ride via ORM

---
+++ src/scripts/createRide.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function createUserRide() {
+  // First, fetch the user by email to get the ID
+  const user = await prisma.user.findUnique({
+    where: {
+      email: 'elsa@prisma.io'
+    }
+  });
+
+  if (!user) {
+    console.log('User not found!');
+    return;
+  }
+
+  // Now create a ride for this user
+  const ride = await prisma.ride.create({
+    data: {
+      title: 'Magical Mountain Adventure',
+      creatorId: user.id
+    }
+  });
+
+  console.log(ride);
+}
+
+createUserRide()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
