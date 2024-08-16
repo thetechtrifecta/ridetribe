11cb0760756c59c42285cef3dba0c791c6ec181d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-08-16 16:55:28 -0700

    reading ride title on FE from ORM via BE Prisma w/ PG

--- src/components/UserSessionInfo.tsx
+++ src/components/UserSessionInfo.tsx
@@ -2,16 +2,25 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import { currentUser } from "@clerk/nextjs/server";
 import CreateRide from './CreateRide';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export default async function UserSessionInfo() {
   const user = await currentUser()
   if (!user) return
+  const ride = await prisma.ride.findFirst({
+    where: { creatorId: 1 },
+  })
+
   return (
     <>
       <Typography>
         Hello {user?.firstName} with primary email {user?.primaryEmailAddress?.emailAddress}
       </Typography>
       <CreateRide />
+      <Typography>
+        {JSON.stringify(ride, null)}
+      </Typography>
     </>
   );
 }
