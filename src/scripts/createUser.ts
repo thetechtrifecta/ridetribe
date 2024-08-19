f664828862264e357fe29b88ba0d323aa4976c5a
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-08-19 13:24:50 -0700

    resolving createUser script

--- src/scripts/createUser.ts
+++ src/scripts/createUser.ts
@@ -4,7 +4,11 @@ const prisma = new PrismaClient();
 async function main() {
   const user = await prisma.user.create({
     data: {
-      email: 'elsa@prisma.io',
+      clerkUserId: 'user_fakeUser1234IDinClerk',
+      email: 'example@example.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      phone: '+11234567890'
     },
   });
   console.log(user);
