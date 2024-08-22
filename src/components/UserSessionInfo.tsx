180a9c316923b2436a51e349c611555ab36a6044
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-08-21 19:00:59 -0700

    refactoring and tested successfully

--- src/components/UserSessionInfo.tsx
+++ src/components/UserSessionInfo.tsx
@@ -12,26 +12,28 @@ import CreateRideAll from './CreateRideAll';
 export default async function UserSessionInfo() {
   const user = await currentUser()
   if (!user) return
-  fetch('/api/sync-user', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      clerkUserId: user.id,
-      email: user.primaryEmailAddress?.emailAddress,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      phone: user.primaryPhoneNumber?.phoneNumber,
-    })
-  })
-  .then(response => response.json()) // Convert the response to JSON
-  .then(data => console.log('User sync response:', data))
-  .catch(error => console.error('Error syncing user:', error));  
+  
+  // Synchronize the user with the database
+  try {
+    const upsertUser = await prisma.user.upsert({
+      where: { clerkUserId: user.id },
+      update: {}, // No updates if user exists
+      create: {
+        clerkUserId: user.id,
+        email: user.primaryEmailAddress?.emailAddress || '',
+        firstName: user.firstName || 'Unknown',
+        lastName: user.lastName || 'Unknown',
+        phone: user.primaryPhoneNumber?.phoneNumber || 'Not Provided',
+      },
+    });
+    console.log('User synchronized successfully:', upsertUser);
+  } catch (error) {
+    console.error('Error syncing user:', error);
+  }
 
-  const ride = await prisma.ride.findFirst({
-    where: { creatorId: 1 },
-  })
+  // const ride = await prisma.ride.findFirst({
+  //   where: { creatorId: 1 },
+  // })
 
   return (
     <>
