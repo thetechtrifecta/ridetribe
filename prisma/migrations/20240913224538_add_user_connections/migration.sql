f478ddfdd37f36730a5ea39502d0c551d040d1ea
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-09-13 15:46:30 -0700

    including self-referencing many-to-many relationship for user connections

---
+++ prisma/migrations/20240913224538_add_user_connections/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "_UserConnections" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_UserConnections_AB_unique" ON "_UserConnections"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_UserConnections_B_index" ON "_UserConnections"("B");
+
+-- AddForeignKey
+ALTER TABLE "_UserConnections" ADD CONSTRAINT "_UserConnections_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserConnections" ADD CONSTRAINT "_UserConnections_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
