21907479f731c0b9776bca010809ee6cc476e081
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-09-16 14:01:45 -0700

    redid splash

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Image from 'next/image';
-import logo from '../../public/images/carpooling.png';  
+import logo from '../../public/images/carpooling.png';
+import { Box, Typography, Container, Paper } from '@mui/material';
 
 export default function Splash() {
     return (
-        <Box
-            sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            width: '100vw',
-            }}
-        >
-            <Image src={logo} alt="Logo" width={200} height={200} />
-        </Box>
+        <Container component="main" maxWidth="lg">
+            <Paper elevation={3} sx={{ my: 4, p: 3 }}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                Welcome to Ride Tribe!
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                We know that the management of driving or carpooling your kids is a problem. With RideTribe, we fix that by allowing you to create Rides for your kids with Tribes of parents you trust.
+                </Typography>
+                <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+                    <Image src={logo} alt="Logo" width={200} height={200} />
+                </Box>
+            </Paper>
+        </Container>
     )
 }
