dca869b9ab439d082279ff33d2b00d7ee115466f
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-10-11 13:26:44 -0700

    removing ride description from schema and BE

---
+++ prisma/migrations/20241011202618_remove_ride_description/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Ride` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Ride" DROP COLUMN "description";
