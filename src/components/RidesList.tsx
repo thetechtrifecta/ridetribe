4c0f95ab16221d8ddf802a85df50139e9c9848da
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-10-16 16:37:14 -0700

    commenting out for build

--- src/components/RidesList.tsx
+++ src/components/RidesList.tsx
@@ -34,57 +34,58 @@ const RidesList: React.FC = () => {
     //     fetchRides();
     // }, []);
 
-    const handleOpenUpdateDialog = (ride: Ride) => {
-        setSelectedRide(ride);
-        setOpenUpdateDialog(true);
-    };
+    // const handleOpenUpdateDialog = (ride: Ride) => {
+    //     setSelectedRide(ride);
+    //     setOpenUpdateDialog(true);
+    // };
 
-    const handleCloseUpdateDialog = () => {
-        setOpenUpdateDialog(false);
-        setSelectedRide(null);
-    };
+    // const handleCloseUpdateDialog = () => {
+    //     setOpenUpdateDialog(false);
+    //     setSelectedRide(null);
+    // };
 
-    const handleSaveUpdate = () => {
-        handleCloseUpdateDialog();
-        // fetchRides();
-    };
+    // const handleSaveUpdate = () => {
+    //     handleCloseUpdateDialog();
+    //     // fetchRides();
+    // };
 
     return (
-        <Box sx={{ margin: 2 }}>
-            {rides.map((group, index) => (
-                <Box key={index} sx={{ marginBottom: 4 }}>
-                    <Paper elevation={2} sx={{
-                        padding: 2,
-                        backgroundColor: theme.palette.background.default,
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center', // This centers the children horizontally
-                        gap: theme.spacing(1)
-                    }}>
-                        <EventNoteIcon sx={{ color: theme.palette.primary.main }} />
-                        <Typography variant="h6">
-                            {group.eventTitle} - {new Date(group.date).toLocaleDateString()}
-                        </Typography>
-                    </Paper>
-                    <Grid container spacing={2} justifyContent="space-around">
-                        <Grid item xs={12} sm={6}>
-                            {group.to && <RenderRideDetails ride={group.to} onOpenUpdateDialog={handleOpenUpdateDialog} />}
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            {group.from && <RenderRideDetails ride={group.from} onOpenUpdateDialog={handleOpenUpdateDialog} />}
-                        </Grid>
-                    </Grid>
-                </Box>
-            ))}
-            {selectedRide && (
-                <UpdateRide
-                    ride={selectedRide}
-                    open={openUpdateDialog}
-                    onClose={handleCloseUpdateDialog}
-                    onSave={handleSaveUpdate}
-                />
-            )}
-        </Box>
+        <div>Hi</div>
+        // <Box sx={{ margin: 2 }}>
+        //     {rides.map((group, index) => (
+        //         <Box key={index} sx={{ marginBottom: 4 }}>
+        //             <Paper elevation={2} sx={{
+        //                 padding: 2,
+        //                 backgroundColor: theme.palette.background.default,
+        //                 display: 'flex',
+        //                 alignItems: 'center',
+        //                 justifyContent: 'center', // This centers the children horizontally
+        //                 gap: theme.spacing(1)
+        //             }}>
+        //                 <EventNoteIcon sx={{ color: theme.palette.primary.main }} />
+        //                 <Typography variant="h6">
+        //                     {group.eventTitle} - {new Date(group.date).toLocaleDateString()}
+        //                 </Typography>
+        //             </Paper>
+        //             <Grid container spacing={2} justifyContent="space-around">
+        //                 <Grid item xs={12} sm={6}>
+        //                     {group.to && <RenderRideDetails ride={group.to} onOpenUpdateDialog={handleOpenUpdateDialog} />}
+        //                 </Grid>
+        //                 <Grid item xs={12} sm={6}>
+        //                     {group.from && <RenderRideDetails ride={group.from} onOpenUpdateDialog={handleOpenUpdateDialog} />}
+        //                 </Grid>
+        //             </Grid>
+        //         </Box>
+        //     ))}
+        //     {selectedRide && (
+        //         <UpdateRide
+        //             ride={selectedRide}
+        //             open={openUpdateDialog}
+        //             onClose={handleCloseUpdateDialog}
+        //             onSave={handleSaveUpdate}
+        //         />
+        //     )}
+        // </Box>
     );
 };
 
